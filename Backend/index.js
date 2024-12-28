cc940f3dae2f151c438acadfe4625f036cd29836
sridevi <srideviav456@gmail.com>
2024-12-28 07:39:04 +0530

    Ignoring cors

--- Backend/index.js
+++ Backend/index.js
@@ -7,11 +7,13 @@ const app = express();
 const DB_URL = process.env.DB_URL; 
 const PORT = process.env.PORT;
 
-app.use(cors({
+app.use(cors(
+/*{
   origin: 'http://localhost:3000',  
   methods: 'GET,POST,PUT,DELETE',  
   allowedHeaders: 'Content-Type,Authorization',  
-}));
+}*/
+));
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.json());
 app.use("/user",UserRoutes);
